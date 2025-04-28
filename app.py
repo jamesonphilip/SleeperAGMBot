422b736f50b7997f7c4790d3694ab6358f69b475
jamesonphilip <36869227+jamesonphilip@users.noreply.github.com>
2025-04-28 14:15:22 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -8,6 +8,30 @@ from bs4 import BeautifulSoup
 import lxml
 from fpdf import FPDF
 
+# --- Add this near the top of app.py ---
+
+def check_password():
+    """Simple password protection."""
+    def password_entered():
+        if st.session_state["password"] == "mysecretpassword":
+            st.session_state["password_correct"] = True
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        st.text_input("Enter password:", type="password", on_change=password_entered, key="password")
+        return False
+    elif not st.session_state["password_correct"]:
+        st.text_input("Enter password:", type="password", on_change=password_entered, key="password")
+        st.error("Password incorrect")
+        return False
+    else:
+        return True
+
+# --- Now protect the app ---
+if not check_password():
+    st.stop()
+
 # --- Configuration ---
 DEEPSEEK_API_KEY = "sk-efec2ddcafba46ff949e25dad349a0c2"
 FANTASYPROS_DYNASTY_RANKINGS_URL = "https://www.fantasypros.com/nfl/rankings/dynasty-overall.php"
